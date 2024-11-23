801e4726e709ae7d6d5ab5205bab8b36fa6c8853
gwangeunahn <gwangeunahn@handong.ac.kr>
2024-11-23 16:44:36 +0900

    #002

--- src/components/Book/Create.js
+++ src/components/Book/Create.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Create() {
   const navigate = useNavigate();
 
+  const titleRef = useRef();
+  const authorRef = useRef();
+  const publisherRef = useRef();
+  const publicationdateRef = useRef();
+
   let defaultCreate = {
-    titel : "",
+    title : "",
     author : "",
     publisher : "",
     publicationdate : ""
@@ -16,6 +21,36 @@ export default function Create() {
   const [bookAdd, setBookAdd] = useState(defaultCreate);
 
   const postBookAdd = () => {
+
+    if (bookAdd.title.trim() === '') {
+      titleRef.current.style.border = '1px solid red';
+      alert("Valid title is required.")
+      return;
+    } else {
+      titleRef.current.style.border = '';
+    }
+    if (bookAdd.author.trim() === '') {
+      authorRef.current.style.border = '1px solid red';
+      alert("Valid author is required.")
+      return;
+    } else {
+      authorRef.current.style.border = '';
+    }
+    if (bookAdd.publisher.trim() === '') {
+      publisherRef.current.style.border = '1px solid red';
+      alert("Valid publisher is required.")
+      return;
+    } else {
+      publisherRef.current.style.border = '';
+    }
+    if (bookAdd.publicationdate.trim() === '') {
+      publicationdateRef.current.style.border = '1px solid red';
+      alert("Valid publicationdate is required.")
+      return;
+    } else {
+      publicationdateRef.current.style.border = '';
+    }
+
     axios.post("https://672c26ca1600dda5a9f76967.mockapi.io/api/v1/books/", bookAdd) 
     .then((response) => {
         console.log(response);
@@ -25,7 +60,7 @@ export default function Create() {
     .catch((error) => {
         console.log(error);
     })
-}
+  }
 
   const onChangeInput = (e) => {
     const { name, value } = e.target;
@@ -35,10 +70,10 @@ export default function Create() {
   return (
     <div className='d-flex justify-content-between'>
         <div className='col-10'>
-            <p>Title : <input type='text' name='title' value={bookAdd.title} onChange={onChangeInput} /></p>
-            <p>Author : <input type='text' name='author' value={bookAdd.author} onChange={onChangeInput} /></p>
-            <p>Publisher : <input type='text' name='publisher' value={bookAdd.publisher} onChange={onChangeInput} /></p>
-            <p>Publicationdate : <input type='date' name='publicationdate' value={bookAdd.publicationdate} onChange={onChangeInput} /></p>  
+            <p>Title : <input type='text' name='title' value={bookAdd.title} onChange={onChangeInput} ref={titleRef} /></p>
+            <p>Author : <input type='text' name='author' value={bookAdd.author} onChange={onChangeInput} ref={authorRef} /></p>
+            <p>Publisher : <input type='text' name='publisher' value={bookAdd.publisher} onChange={onChangeInput} ref={publisherRef} /></p>
+            <p>Publicationdate : <input type='date' name='publicationdate' value={bookAdd.publicationdate} onChange={onChangeInput} ref={publicationdateRef} /></p>  
         </div>
         <div className='col-1'>
             <p><button className='nav-link' onClick={postBookAdd}>Add</button></p> 
--- src/components/Book/Update.js
+++ src/components/Book/Update.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { useState , useEffect } from 'react';
+import { useState , useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export default function Update() {
     const { id } = useParams();
-    
     const [count, setCount] = useState(0);
+
+    const titleRef = useRef();
+    const authorRef = useRef();
+    const publisherRef = useRef();
+    const publicationdateRef = useRef();
 
     let defaultBook = {
         id : "",
@@ -29,13 +33,28 @@ export default function Update() {
         })
     }
     
-    const handleChange = async (e) => {
+    const handleChange = (e) => {
         const { name, value } = e.target;
-        let result = await setBookDetail({ ...bookDetail, [name]: value });
-
         const updatedBookDetail = { ...bookDetail, [name]: value };
+        setBookDetail(updatedBookDetail);
 
-        let result2 = await axios.put("https://672c26ca1600dda5a9f76967.mockapi.io/api/v1/books/" + id, updatedBookDetail) 
+        const refMap = {
+            title: titleRef,
+            author: authorRef,
+            publisher: publisherRef,
+            publicationdate: publicationdateRef,
+        };
+
+        if (value.trim() === '') {
+            refMap[name].current.style.border = '1px solid red';
+            alert("Valid input is required.")
+            setCount(count+1);
+            return;
+        } else {
+            refMap[name].current.style.border = '';
+        }
+
+        axios.put("https://672c26ca1600dda5a9f76967.mockapi.io/api/v1/books/" + id, updatedBookDetail) 
         .then((response) => {
             console.log(response);
             setCount(count+1);
@@ -54,10 +73,10 @@ export default function Update() {
     <div className='d-flex justify-content-between'>
         <div className='col-10'>
             <p>ID : {bookDetail.id}</p>
-            <p>Title : <input type='text' name='title' value={bookDetail.title} onChange={handleChange} /></p>
-            <p>Author : <input type='text' name='author' value={bookDetail.author} onChange={handleChange} /></p>
-            <p>Publisher : <input type='text' name='publisher' value={bookDetail.publisher} onChange={handleChange} /></p>
-            <p>Publicationdate : <input type='date' name='publicationdate' value={bookDetail.publicationdate?.substr(0, 10)} onChange={handleChange} /></p>
+            <p>Title : <input type='text' name='title' value={bookDetail.title} onChange={handleChange} ref={titleRef} /></p>
+            <p>Author : <input type='text' name='author' value={bookDetail.author} onChange={handleChange} ref={authorRef} /></p>
+            <p>Publisher : <input type='text' name='publisher' value={bookDetail.publisher} onChange={handleChange} ref={publisherRef} /></p>
+            <p>Publicationdate : <input type='date' name='publicationdate' value={bookDetail.publicationdate?.substr(0, 10)} onChange={handleChange} ref={publicationdateRef} /></p>
         </div>
         <div className='col-2'>
             <p>COUNT : {count}</p>
